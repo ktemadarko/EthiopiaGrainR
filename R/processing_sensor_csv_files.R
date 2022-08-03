07204f77e56c5d400a6e93a8cd10d11037e1f523
ktemadarko <k.temadarko@gmail.com>
2022-08-03 06:08:34 +0000

    change

--- R/processing_sensor_csv_files.R
+++ R/processing_sensor_csv_files.R
@@ -2,7 +2,7 @@
 
 #X0869----
 X0869 <- readr::read_csv("data-raw/Sensor Data/0869.csv",
-col_types = cols(Timestamp = col_character())) |>
+col_types = readr::cols(Timestamp = col_character())) |>
  dplyr::mutate(sensor_id="X0869", .before=Timestamp) #adding sensor-id column
 #removing excess characters that were imported
 X0869$Timestamp <-stringr::str_sub(X0869$Timestamp,3,21)
@@ -14,7 +14,7 @@ X0869$single_date=lubridate::floor_date(X0869$Timestamp , unit= "day")
 
 #X0868----
 X0868 <- read_csv("data-raw/Sensor Data/0868.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X0868", .before=Timestamp)
 X0868$Timestamp <-stringr::str_sub(X0868$Timestamp,3,21)
 X0868$Timestamp <-readr::parse_datetime(X0868$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -22,7 +22,7 @@ X0868$single_date=lubridate::floor_date(X0868$Timestamp , unit= "day")
 
 #X0862----
 X0862 <- read_csv("data-raw/Sensor Data/0862.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X0862", .before=Timestamp)
 X0862$Timestamp <-stringr::str_sub(X0862$Timestamp,3,21)
 X0862$Timestamp <-readr::parse_datetime(X0862$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -30,7 +30,7 @@ X0862$single_date=lubridate::floor_date(X0862$Timestamp , unit= "day")
 
 #X0861----
 X0861 <- read_csv("data-raw/Sensor Data/0861.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X0861", .before=Timestamp)
 X0861$Timestamp <-stringr::str_sub(X0861$Timestamp,3,21)
 X0861$Timestamp <-readr::parse_datetime(X0861$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -38,7 +38,7 @@ X0861$single_date=lubridate::floor_date(X0861$Timestamp , unit= "day")
 
 #X085c----
 X085c <- read_csv("data-raw/Sensor Data/085c.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X085c", .before=Timestamp)
 X085c$Timestamp <-stringr::str_sub(X085c$Timestamp,3,21)
 X085c$Timestamp <-readr::parse_datetime(X085c$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -46,7 +46,7 @@ X085c$single_date=lubridate::floor_date(X085c$Timestamp , unit= "day")
 
 #X0885e----
 X085e <- read_csv("data-raw/Sensor Data/085e.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X085e", .before=Timestamp)
 X085e$Timestamp <-stringr::str_sub(X085e$Timestamp,3,21)
 X085e$Timestamp <-readr::parse_datetime(X085e$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -55,7 +55,7 @@ X085e$single_date=lubridate::floor_date(X085e$Timestamp , unit= "day")
 
 #X085f----
 X085f <- read_csv("data-raw/Sensor Data/085f.csv",
-                  col_types = cols(Timestamp = col_character())) |>
+                  col_types = readr::cols(Timestamp = col_character())) |>
   dplyr::mutate(sensor_id="X085f", .before=Timestamp)
 X085f$Timestamp <-stringr::str_sub(X085f$Timestamp,3,21)
 X085f$Timestamp <-readr::parse_datetime(X085f$Timestamp ,locale=locale(tz="Africa/Addis_Ababa"))
@@ -84,7 +84,7 @@ X085f$single_date=lubridate::floor_date(X085f$Timestamp , unit= "day")
 #'
 #' @rdname dates_n_labels
 #'
-#' @return
+#' @return add columns year_created
 #'
 #' @export
 #'
